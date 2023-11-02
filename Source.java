54c39417ed3274830a7e3dfeafe0f6e1f81bad26
Herman <julian.herman@fiserv.com>
2023-11-02 13:11:36 -0400

    implemented sumList()

--- Source.java
+++ Source.java
@@ -1,34 +1,54 @@
 class Node<T> {
-  T val;
-  Node<T> next;
+    T val;
+    Node<T> next;
 
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 class Source {
-  public static int sumList(Node<Integer> head) {
-    // todo
-    return;
-  }
+    public static int sumList(Node<Integer> head) {
+        int sum = 0;
+        while (head != null) {
+            sum += head.val;
+            head = head.next;
+        }
+        return sum;
+    }
 
-  public static void main(String[] args) {
-    Node<Integer> a = new Node<>(2);
-    Node<Integer> b = new Node<>(8);
-    Node<Integer> c = new Node<>(3);
-    Node<Integer> d = new Node<>(-1);
-    Node<Integer> e = new Node<>(7);
+    public static void main(String[] args) {
+        Node<Integer> a = new Node<>(2);
+        Node<Integer> b = new Node<>(8);
+        Node<Integer> c = new Node<>(3);
+        Node<Integer> d = new Node<>(-1);
+        Node<Integer> e = new Node<>(7);
 
-    a.next = b;
-    b.next = c;
-    c.next = d;
-    d.next = e;
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
 
-    // 2 -> 8 -> 3 -> -1 -> 7
-    
-    System.out.println("Sum of my list: ");
-    System.out.println(Source.sumList(a));
-  }
+        // 2 -> 8 -> 3 -> -1 -> 7
+
+        System.out.print("Sum of my first list: ");
+        System.out.println(Source.sumList(a));
+
+        a = new Node<>(219);
+        b = new Node<>(81);
+        c = new Node<>(-81);
+        d = new Node<>(-50);
+        e = new Node<>(7);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        // 219 -> 81 -> -81 -> -50 -> 7
+
+        System.out.print("Sum of my second list: ");
+        System.out.println(Source.sumList(a));
+    }
 }
